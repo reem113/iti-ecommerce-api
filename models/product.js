da211a7e9fa05aabe0bf6f6aeb07be08aea3008d
MahmoudElmahalawy <elmehalawy7@gmail.com>
2021-08-05 19:05:38 +0200

    First commit

---
+++ models/product.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	shortDescription: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		default: "",
+	},
+	images: [
+		{
+			type: String,
+		},
+	],
+	brand: {
+		type: String,
+		default: "Generic",
+	},
+	price: {
+		type: Number,
+		required: true,
+	},
+	category: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Category",
+		required: true,
+	},
+	countInStock: {
+		type: Number,
+		required: true,
+		min: 0,
+		max: 250,
+	},
+	reviews: [
+		{
+			type: String,
+		},
+	],
+	rating: {
+		type: Number,
+		default: 0,
+	},
+	isFeatured: {
+		type: Boolean,
+		default: false,
+	},
+	dateCreated: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
