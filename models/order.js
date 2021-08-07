3fac577c7929feaa6648c160a3403497b3f648ea
MahmoudElmahalawy <elmehalawy7@gmail.com>
2021-08-07 18:16:18 +0200

    Created order and orderItem models

---
+++ models/order.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+	orderItems: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "OrderItem",
+			required: true,
+		},
+	],
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+		required: true,
+	},
+	shippingAddress: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Address",
+		required: true,
+	},
+	phone: {
+		type: String,
+		required: true,
+	},
+	totalPrice: {
+		type: Number,
+		required: true,
+	},
+	status: {
+		type: String,
+		default: "Pending",
+	},
+	dateOrdered: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
---
+++ models/orderItem.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const orderItemSchema = new mongoose.Schema({
+	product: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Product",
+	},
+	quantity: {
+		type: Number,
+		required: true,
+	},
+});
+
+const OrderItem = mongoose.model("OrderItem", orderItemSchema);
+
+module.exports = OrderItem;
