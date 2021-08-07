db35dbbe0c87364b9d6ef48e679c7d8c841ebfe5
MahmoudElmahalawy <elmehalawy7@gmail.com>
2021-08-07 02:36:09 +0200

    Execluding authentication for accessing public routes

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -4,6 +4,13 @@ const authJwt = () => {
 	return expressJwt({
 		secret: process.env.JWT_SECRET,
 		algorithms: ["HS256"],
+	}).unless({
+		path: [
+			{ url: /\/api\/v1\/products(.*)/, method: ["GET", "OPTIONS"] },
+			{ url: /\/api\/v1\/categories(.*)/, method: ["GET", "OPTIONS"] },
+			`${process.env.API_URL}/users/login`,
+			`${process.env.API_URL}/users/register`,
+		],
 	});
 };
 
